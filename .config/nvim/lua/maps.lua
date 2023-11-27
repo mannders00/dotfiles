e7498ede9e8ed4739162a0ea032ea05045a115b2
Matt Anderson <matta9001@gmail.com>
2023-11-27 13:31:21 -0600

    HJKL in normal mode now resizes windows by 5

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -4,4 +4,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-l>', builtin.find_files, {})
 vim.keymap.set('n', '<C-k>', builtin.live_grep, {})
 
+vim.api.nvim_set_keymap('n', 'H', '5<C-w><', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'J', '5<C-w>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'K', '5<C-w>-', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'L', '5<C-w>>', { noremap = true, silent = true })
+
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
