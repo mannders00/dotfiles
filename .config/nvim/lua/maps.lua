8c1c88840ddc40b326c2dab83f225a2e5e2a4b62
Matt Anderson <matta9001@gmail.com>
2024-01-02 12:27:14 -0600

    Removed keybinds for resizing because it actually broke my old telescope bindings

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,13 +1,11 @@
 vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>cd', ':cd %:p:h<CR>:pwd<CR>')
+
 vim.keymap.set('n', '<C-l>', builtin.find_files, {})
 vim.keymap.set('n', '<C-k>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.commands, {})
-
-vim.keymap.set('n', '<C-H>', ':vertical resize -5<CR>', { silent = true })
-vim.keymap.set('n', '<C-L>', ':vertical resize +5<CR>', { silent = true })
-vim.keymap.set('n', '<C-J>', ':resize -5<CR>', { silent = true })
-vim.keymap.set('n', '<C-K>', ':resize +5<CR>', { silent = true })
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
