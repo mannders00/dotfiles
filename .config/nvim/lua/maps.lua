785048b7d1cd0889e85a95dba29a6c1566a8f26c
Matt Anderson <matta9001@gmail.com>
2024-06-20 14:42:20 -0500

    Update maps.lua

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -8,3 +8,14 @@ vim.keymap.set('n', '?', ':WhichKey<CR>', {})
 vim.keymap.set('i', '<C-e>', 'if err != nil {}<Left>', { noremap = true })
 
 vim.cmd [[autocmd BufWritePre *.go lua vim.lsp.buf.format()]]
+
+-- Change Tabs Quickly
+vim.keymap.set('n', '<leader>1', '1gt', {})
+vim.keymap.set('n', '<leader>2', '2gt', {})
+vim.keymap.set('n', '<leader>3', '3gt', {})
+vim.keymap.set('n', '<leader>4', '4gt', {})
+vim.keymap.set('n', '<leader>5', '5gt', {})
+vim.keymap.set('n', '<leader>6', '6gt', {})
+vim.keymap.set('n', '<leader>7', '7gt', {})
+vim.keymap.set('n', '<leader>8', '8gt', {})
+vim.keymap.set('n', '<leader>9', '9gt', {})
