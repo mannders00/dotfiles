926b157e6d14e84fc8c9115e87c3a629aeaba31e
Matt Anderson <matta9001@gmail.com>
2024-06-20 14:57:22 -0500

    Update maps.lua

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,3 +1,40 @@
+-- Function to enter resize mode
+function enter_resize_mode()
+  local keymap_opts = { noremap = true, silent = true }
+  
+  -- Map hjkl to resize panes
+  vim.api.nvim_set_keymap('n', 'h', ':vertical resize -2<CR>', keymap_opts)
+  vim.api.nvim_set_keymap('n', 'j', ':resize +2<CR>', keymap_opts)
+  vim.api.nvim_set_keymap('n', 'k', ':resize -2<CR>', keymap_opts)
+  vim.api.nvim_set_keymap('n', 'l', ':vertical resize +2<CR>', keymap_opts)
+  
+  -- Map <Esc> to exit resize mode
+  vim.api.nvim_set_keymap('n', '<Esc>', ':lua exit_resize_mode()<CR>', keymap_opts)
+
+  -- Show message indicating resize mode
+  vim.api.nvim_echo({{"Entering Resize Mode. Press <Esc> to exit.", "WarningMsg"}}, false, {})
+end
+
+-- Function to exit resize mode
+function exit_resize_mode()
+  -- Unmap hjkl
+  vim.api.nvim_del_keymap('n', 'h')
+  vim.api.nvim_del_keymap('n', 'j')
+  vim.api.nvim_del_keymap('n', 'k')
+  vim.api.nvim_del_keymap('n', 'l')
+  
+  -- Unmap <Esc>
+  vim.api.nvim_del_keymap('n', '<Esc>')
+
+  -- Clear the message indicating resize mode
+  vim.api.nvim_echo({{"Exiting Resize Mode.", "WarningMsg"}}, false, {})
+  vim.defer_fn(function() vim.api.nvim_command('echo ""') end, 2000) -- Clear the message after 2 
+end
+
+-- Map leader key to enter resize mode
+vim.api.nvim_set_keymap('n', '<leader>r', ':lua enter_resize_mode()<CR>', { noremap = true, silent = true })
+
+-- Map Ctrl+Shift+h/j/k/l to resize panes
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-b>', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', builtin.find_files, {})
