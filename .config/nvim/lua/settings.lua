c4956b70e5a28e3cce23a8f48568fcd2a5daedd6
Matt Anderson <matta9001@gmail.com>
2024-09-16 09:58:37 -0500

    Update settings.lua

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -4,6 +4,7 @@ vim.o.shiftwidth = 4
 vim.o.autoindent = true
 vim.o.foldmethod = "indent"
 vim.o.foldlevel = 99
+vim.o.wrap = false
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.g.mapleader = ' '
