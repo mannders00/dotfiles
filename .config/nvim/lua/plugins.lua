e06a4ba9dd2391005a4711f1a0bc6746f3db185e
Matt Anderson <matta9001@gmail.com>
2024-12-04 12:27:36 -0600

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -66,7 +66,13 @@ require('nnn').setup({
 		fullscreen = false,
 	},
 })
+local function tree_on_attach(bufnr)
+	local api = require('nvim-tree.api')
+	api.config.mappings.default_on_attach(bufnr)
+	vim.keymap.del('n', '<C-e>', { buffer = bufnr })
+end
 require('nvim-tree').setup({
+	on_attach = tree_on_attach,
 	view = {
 		side = "right"
 	},
