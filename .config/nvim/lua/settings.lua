fb4ec6ecb81035e7f1670953dff69b472500496f
Matt Anderson <matta9001@gmail.com>
2025-05-08 16:02:59 +0100

    apply colors in autogroup after colorscheme applied

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -10,28 +10,20 @@ vim.wo.relativenumber = true
 vim.g.mapleader = ' '
 -- vim.o.clipboard="unnamedplus"
 
-vim.cmd [[
+vim.cmd([[
   colorscheme default
   syntax on
   set notermguicolors
 
-  " magenta for control‐flow and other keywords
-  highlight Statement cterm=bold    ctermfg=5
-  highlight Keyword   ctermfg=5
-
-  " cyan for types (struct, interface, custom types)
-  highlight Type      ctermfg=6
-
-  " bold blue for function names
-  highlight Function  cterm=bold    ctermfg=4
-
-  " yellow for literals/constants
-  highlight Constant  ctermfg=3
-
-  " green for strings
-  highlight String    ctermfg=2
-
-  " italic grey for comments (bright black)
-  " highlight Comment   cterm=italic  ctermfg=lightgray
-]]
+  augroup MyColors
+    autocmd!
+    autocmd ColorScheme * highlight Statement cterm=bold    ctermfg=5
+    autocmd ColorScheme * highlight Keyword   ctermfg=5
+    autocmd ColorScheme * highlight Type      ctermfg=6
+    autocmd ColorScheme * highlight Function  cterm=bold    ctermfg=4
+    autocmd ColorScheme * highlight Constant  ctermfg=3
+    autocmd ColorScheme * highlight String    ctermfg=2
+    " autocmd ColorScheme * highlight Comment   cterm=italic  ctermfg=lightgray
+  augroup END
+]])
 
