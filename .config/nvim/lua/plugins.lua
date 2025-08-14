4616323137cafaf22ce9288f1b40a79e3674af60
Matt Anderson <matta9001@gmail.com>
2025-08-14 16:01:10 +0100

    update org dir

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -70,8 +70,8 @@ require('lazy').setup({
 		config = function()
 			-- Setup orgmode
 			require('orgmode').setup({
-				org_agenda_files = '~/orgfiles/**/*',
-				org_default_notes_file = '~/orgfiles/refile.org',
+				org_agenda_files = '~/org/**/*',
+				org_default_notes_file = '~/org/refile.org',
 			})
 
 			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
